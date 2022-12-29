592ad7659faf1ded745543986570e6f14bb1be76
Tanguy Pinel <tanguypinel@gmail.com>
2022-12-29 11:19:42 +0100

    Nav Mobile

--- header.php
+++ header.php
@@ -28,7 +28,7 @@
 <body <?php body_class(); ?>>
 	<?php wp_body_open(); ?>
 	<div id="page" class="site">
-		<a class="skip-link screen-reader-text" href="#main-content"><?php esc_html_e('Aller au contenu', 'guzeldiag'); ?></a>
+		<!-- <a class="skip-link screen-reader-text" href="#main-content"><?php esc_html_e('Aller au contenu', 'guzeldiag'); ?></a> -->
 
 		<div class="hidden" hiddden>
 			<?php get_template_part( 'dist/img/sprite.svg' ); ?>
--- template-parts/navigation/primary.php
+++ template-parts/navigation/primary.php
@@ -1,17 +1,28 @@
 <?php
+
 /**
  * Primary menu template part
- */    
-?>   
+ */
+?>
 
-<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Primary', 'guzeldiag' ); ?>">
-    <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><p><?php esc_html_e( 'Menu', 'guzeldiag' ); ?></p><span></span></button>
-    <?php
+<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e('Primary', 'guzeldiag'); ?>">
+    <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><span></span></button>
+    <?php the_custom_logo(); ?>
+    <div class="menu-principal">
+        <?php
+        $page = get_page_by_title('accueil');
+        $ids = "{$page->ID}"; //Exlude page from the menu
         wp_nav_menu(
             array(
+                'menu' => 'menu-principal',
                 'theme_location' => 'menu-1',
-                'menu_id'        => 'primary-menu',
+                'exclude' => $ids
             )
         );
-    ?>
+        $path = get_template_directory_uri();
+        ?>
+        <div class="menu-principal-logo">
+            <a href="<?= get_home_url(); ?>" alt="Accueil">
+                <img src="<?= $path; ?>/dist/img/LOGO-GUZELDIAG-white.webp" /></a>
+        </div>
 </nav><!-- #site-navigation -->
